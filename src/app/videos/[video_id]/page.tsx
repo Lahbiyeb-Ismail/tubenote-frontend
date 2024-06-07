9cd619ef22d792315d8e573984e96efb601917a4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-06-07 16:36:47 +0100

    feat(UI): add sidebar component to the video page

--- src/app/videos/[video_id]/page.tsx
+++ src/app/videos/[video_id]/page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import Sidebar from "@/components/global/sideBar";
+
 function VideoPage() {
   return (
-    <main className="flex flex-col items-center justify-center bg-slate-50">
-      videoPage
+    <main className="flex h-screen bg-slate-50">
+      <Sidebar />
+      <div className="flex-1 p-10">
+        <div>
+          <div>video player</div>
+          <div>Text editor</div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/global/sideBar.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+function Sidebar() {
+  return (
+    <nav className="flex h-screen w-1/5 flex-col items-center justify-between gap-10 border-r border-gray-200  px-4 py-6 dark:bg-black">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Chapters</AccordionTrigger>
+          <AccordionContent className="pl-2">
+            <Accordion type="single" collapsible>
+              <AccordionItem value="item-1-1">
+                <AccordionTrigger>
+                  Chapter 1 - 00:00 -&gt; 10:00
+                </AccordionTrigger>
+                <AccordionContent className="pl-2">
+                  Chapter 1 content
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem value="item-1-2">
+                <AccordionTrigger>
+                  Chapter 2 - 10:00 -&gt; 20:00
+                </AccordionTrigger>
+                <AccordionContent className="pl-2">
+                  Chapter 2 content
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </nav>
+  );
+}
+
+export default Sidebar;
